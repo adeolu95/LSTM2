5cd91f2914ad9911c822749e0405ca2ab4d388d1
Adeoluwa Adeleke <adeolu1995adeleke@gmail.com>
2024-11-28 19:37:21 -0500

    More methods installed
    Lstm model taking shape

--- LSTM2/src/LSTM.java
+++ LSTM2/src/LSTM.java
@@ -36,9 +36,120 @@ public class LSTM {
         bi = randomVector(hiddenSize, scale);
         bc = randomVector(hiddenSize, scale);
         bo = randomVector(hiddenSize, scale);
+
+
+        //Iniitialization of States
+        h=  randomVector(hiddenSize, 0.0);
+        c=  randomVector(hiddenSize, 0.0);
+
+
         
 
     }
+    // Model Training method
+
+    public void train(double[][] data, int timeSteps, int epochs, double learningRate ){
+        // prepare data
+        double[][][] inputSequences = createInputSequences(data,timeSteps);
+        double[][] targets = createTargets(data, timeSteps,3);
+
+        for(int epoch =0; epoch<epochs; epoch++){
+            double loss =0.0;
+            
+
+            for(int i=0; i<inputSequences.length; i++){
+                // forward pass 
+                double[] prediction = forwardPass(inputSequences[i]);
+
+                // Compute Loss
+                loss+=Math.pow(targets[i][0]-prediction[0],2);
+
+
+            }
+
+            loss/= inputSequences.length;
+            System.out.println("Epoch "+ (epoch+1)+ ", Loss: "+ loss);
+        }
+
+    }
+
+    private double[][][] createInputSequences(double[][] data, int timeSteps){
+        int numSequences = data.length - timeSteps;
+        int features = data[0].length;
+        double[][][] inputSequences = new double[numSequences][timeSteps][features];
+
+        for(int i =0; i< numSequences; i++){
+            for(int j =0; j< timeSteps; j++){
+                inputSequences[i][j] = data[i+j];
+            }
+        }
+        return inputSequences;
+    }
+
+    private double[][] createTargets(double[][] data, int timeSteps, int targetFeatureIndex){
+        int numTargets = data.length - timeSteps;
+        double[][] targets = new double[numTargets][1];
+
+        for(int i=0; i<numTargets; i++){
+            targets[i][0]= data[i+timeSteps][targetFeatureIndex];
+        }
+
+        return targets;
+    }
+
+    private double[] forwardPass(double[][] sequence){
+        double[][] hiddenStates =  new double[sequence.length][hiddenSize];
+        double[][] cellStates = new double[sequence.length][hiddenSize];
+
+        for(int t=0; t<sequence.length; t++){
+            double [] xt = sequence[t];
+            //initializing ht-1 and ct-1
+            double [] htPrev = (t==0) ? new double [hiddenSize]: hiddenStates[t-1];
+            double [] ctPrev = (t==0) ? new double [hiddenSize]: cellStates[t-1];
+
+            double [] concat = concatArrays(htPrev,xt);
+            // Compute gates
+            double[] inputGate = sigmoidVector(matVecMul(Wi, concat, bi));
+            double[] forgetGate = sigmoidVector(matVecMul(Wf, concat, bf));
+            double[] outputGate = sigmoidVector(matVecMul(Wo, concat, bo));
+
+            //update cell state
+            double[] candidateCell = tanhVector(matVecMul(Wc, concat, bc));
+            double[] cellState = vectorAdd(vectorMultiply(forgetGate, ctPrev), vectorMultiply(inputGate, candidateCell));
+
+            cellStates[t] = cellState;
+
+            //compute hidden state
+            double[] hiddenState = vectorMultiply(outputGate,tanhVector(cellState));
+            hiddenStates[t] = hiddenState;
+        }
+
+        // Return final output
+        return hiddenStates[hiddenStates.length-1];
+    }
+
+    private double computeLoss(double[][] predictions, double[][] targets){
+        double loss = 0.0;
+        int n = predictions.length;
+
+        for(int i =0; i<n; i++){
+            loss+= Math.pow(targets[i][0]-predictions[i][0],2);
+        }
+
+        return loss/n;
+    }
+
+    private void updateWeight(double[][] gradients, double[][] weights, double learningRate){
+        for (int i =0; i<weights.length; i++){
+            for (int j =0; j < weights[0].length; j++){
+                weights[i][j]-= learningRate*gradients[i][j];
+            }
+        }
+    }
+
+
+
+    //Internal Class Methods
 
     
     // Sigmoid activation function
--- LSTM2/src/preProcessing.java
+++ LSTM2/src/preProcessing.java
@@ -12,6 +12,7 @@ public class preProcessing {
             this.numberFeatures = input.get(0).length;
 
             for (int i =0; i<input.size(); i++){
+                
 
 
                 
@@ -26,5 +27,48 @@ public class preProcessing {
         }
 
     }
+
+    private double[][] normalizeData(ArrayList<double[]> input){
+        int rows = input.size();
+        int cols = input.get(0).length;
+
+        double[] min = new double[cols];
+        double[] max = new double[cols];
+
+        // initialize min and max arrays
+        for(int j= 0; j<cols; j++){
+            min[j] = Double.MAX_VALUE;
+            max[j] = Double.MIN_VALUE;
+        }
+
+        //compute min and max for each feature
+        for(int i=0; i< rows; i++){
+            for(int j =0; j<cols; j++){
+                if(input.get(i)[j]< min[j]) min[j] = input.get(i)[j];
+                if(input.get(i)[j]> max[j]) max[j] = input.get(i)[j];
+
+            }
+        }
+
+        double[][] normalizedData = new double[rows][cols];
+        for(int i =0; i< rows; i++){
+            for(int j=0; j<cols; j++){
+                normalizedData[i][j]= (input.get(i)[j]-min[j])/(max[j]-min[j]);
+            }
+        }
+
+        return normalizedData;
+    }
+
+    private double[] denormalize(double[] normalized, double min, double max){
+        double [] original = new double[normalized.length];
+        for (int i =0; i< normalized.length; i++){
+            original[i] = normalized[i]*(max-min)+min;
+        }
+        return original;
+    }
+
+
+
     
 }
